859cd0c5f6335ba24890e7e4f8e938766b6b6f7c
jimkel97 <jim.brothers@live.com>
2018-11-15 20:54:11 -0500

    Delete displayDate.js

--- displayDate.js
+++
@@ -1,6 +0,0 @@
-var today = new Date();
-var day = today.getDay();
-var month = today.getMonth();
-var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-var monthsOfYear = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-alert(daysOfWeek[day] + ", " + monthsOfYear[month] + " " + today.getDate() + ", " + today.getFullYear());
